d7a1c4d34f410dee87923ebc68afc3264563849c
RSooriya <soormo06@gmail.com>
2020-06-30 13:25:46 +0530

    Comments added

--- src/com/sooriya/MyApp.java
+++ src/com/sooriya/MyApp.java
@@ -3,6 +3,10 @@ package com.sooriya;
 public class MyApp {
 
 	public static void main(String[] args) {
+		
+		  // using code to an interface instead of directly creating its own reference
+		  // so we can plug in any type of class that implements the interface
+		  // Code to an interface - best software engineering practice
           Coach theCoach = new TrackCoach();
           System.out.println(theCoach.getDailyWorkout());
 	}
